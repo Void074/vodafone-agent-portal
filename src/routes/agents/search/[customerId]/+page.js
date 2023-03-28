337badea22446d08bf34dba8efd1bc4a7710b1f0
Gedare Dorke <devpro074@gmail.com>
2023-03-29 09:37:44 +1000

    Minor Update v1.6.0   * new     - view customer profile directly on search page   * refactored     - fixed format on both view profile pages in the search and       dashboard

---
+++ src/routes/agents/search/[customerId]/+page.js
@@ -0,0 +1,12 @@
+export const load = ({ params, fetch }) => {
+  const fetchProfile = async(id) => {
+    const res = await fetch(`api/customers/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+  return {
+    profile: fetchProfile(params.customerId)
+  }
+}
